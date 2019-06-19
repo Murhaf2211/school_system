300a612cee374fdc33a39bc859ab467d705fa312
unknown <orfali.murhaf@web.de>
2019-06-19 19:31:48 +0200

    clone the new master and solve all problem

--- browser/src/App.js
+++ browser/src/App.js
@@ -1,41 +1,23 @@
+import React,{Component} from 'react';
 import {BrowserRouter ,NavLink ,Route ,Redirect} from 'react-router-dom';
 import Navbare from './components/Navbar';
-import Teacher from './components/TeacherPage';
-import Student from './components/StudentPage';
-import React,{Component} from 'react';
+import {TeacherContainer} from './components/TeacherPage';
+import {StudentContainer} from './components/StudentPage';
 import logo from './logo.svg';
 import './App.css';
-import {LoginFormContainer} from './components/form';
 import SlideImages from './components/SlideImages';
-import AdminPage from './components/adminPage';
-import ClassInfo from './components/ClassInfo.js';
-import ChangeRemoveClass from './components/changeName_removeClass.js';
-import {RemoveClass} from './components/changeName_removeClass.js';
-import ChangeTeacher from './components/changeTeacher.js';
-import  AddStudent from './components/add_reomve_students.js';
-
 
 
 
 export default class App extends Component {
   render() {
     return (
-      <>
-
       <BrowserRouter>
-
-    < Route path ="/" exact component={ LoginFormContainer} />
-
-    < Route path ="/" component={ SlideImages} />
-    < Route path ="/adminpage" exact component={ AdminPage} />
-
-
-
-
-       <SlideImages/>
-
+        <Route path="/"  component={Navbare}/>
+        <Route path="/"  component={SlideImages}/>
+        <Route path="/teacher" exact component={TeacherContainer} />
+        <Route path="/student"  exact component={StudentContainer} />
       </BrowserRouter>
-      </>
     )
   }
 }
--- browser/src/redux/redux.js
+++ browser/src/redux/redux.js
@@ -1,90 +1,75 @@
-
-import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 
-const initialState = {dataFromServer:[],
-                     TeacherEvaluation:[],
-                     courseEvaluation:[],
-                     studentComent:[],
-                     userNameValue:'',
-                     passwordValue:'',
-                     loginRedirection:false};
+const initialState = {dataFromServer:[], TeacherEvaluation:[],courseEvaluation:[],studentComent:[]};
 
 const reducer = (state = initialState, action) => {
- const copyOfState = {...state};
+  const copyOfState = {...state};
 
- switch (action.type) {
-   case 'updateCOURS':
-     const changeCourseEv = action.event.target;
-     updateCourseEV.courseEvaluation[changeCourseEv]= action.event.target.value;
-     return copyOfState;
-   case 'updateTCHER':
-     const changeTeacherEV = action.event.target;
-     updateTeacherEV.TeacherEvaluation[changeTeacherEV]= action.event.target.value;
-     return copyOfState;
-   case 'updateSTcoment':
-     const changeStudentCom = action.event.target;
-     updateStudentComnt.studentComent[changeStudentCom]= action.event.target.value;
-     return copyOfState;
-   case 'FETCHDATA':
-     copyOfState.dataFromServer = '';
-     return copyOfState;
-   case 'ERROR' :
-     copyOfState.dataFromServer = action.payload.message;
-     return copyOfState;
-   default:
-     return copyOfState;
- }
+  switch (action.type) {
+    case 'updateCOURS':
+      const changeCourseEv = action.event.target;
+      updateCourseEV.courseEvaluation[changeCourseEv]= action.event.target.value;
+      return copyOfState;
+    case 'updateTCHER':
+      const changeTeacherEV = action.event.target;
+      updateTeacherEV.TeacherEvaluation[changeTeacherEV]= action.event.target.value;
+      return copyOfState;
+    case 'updateSTcoment':
+      const changeStudentCom = action.event.target;
+      updateStudentComnt.studentComent[changeStudentCom]= action.event.target.value;
+      return copyOfState;
+    case 'FETCHDATA':
+      copyOfState.dataFromServer = '';
+      return copyOfState;
+    case 'ERROR' :
+      copyOfState.dataFromServer = action.payload.message;
+      return copyOfState;
+    default:
+      return copyOfState;
+  }
 }
 
 export const updateCourseEV = ev => {
- return{type:'updateCOURS', event: ev}
+  return{type:'updateCOURS', event: ev}
 }
 export const updateTeacherEV = ev => {
- return{type:'updateTCHER', event: ev}
+  return{type:'updateTCHER', event: ev}
 }
 export const updateStudentComnt = ev => {
- return{type:'updateSTcoment', event: ev}
+  return{type:'updateSTcoment', event: ev}
 }
 
 const allData = data => {
- return{
-   type: 'FETCHDATA',
-   payload :data
- }
+  return{
+    type: 'FETCHDATA',
+    payload :data
+  }
 }
 
 const badRequest = error => {
- return {
-   type: 'ERROR',
-   payload :error
- }
-}
-
-
-
-export const changeAction = payload => {
-  return { type: 'CHANGE', payload: payload }
+  return {
+    type: 'ERROR',
+    payload :error
+  }
 }
 
 export const fetchFrom = () => {
- return function (dispatch){
-   fetch('')
-   .then(res => {
-     if(res.status >= 400 && res.status <500){
-       throw new Error('Error! check your functions');
-     }else{
-       return res.json();
-     }
-   })
-   .then(dataObject => {
-     dispatch(allData(dataObject));
-   })
-   .catch(err => {
-     dispatch(badRequest(err))
-   })
- }
+  return function (dispatch){
+    fetch('')
+    .then(res => {
+      if(res.status >= 400 && res.status <500){
+        throw new Error('Error! check your functions');
+      }else{
+        return res.json();
+      }
+    })
+    .then(dataObject => {
+      dispatch(allData(dataObject));
+    })
+    .catch(err => {
+      dispatch(badRequest(err))
+    })
+  }
 }
 export const store = createStore(reducer, applyMiddleware(thunk));
