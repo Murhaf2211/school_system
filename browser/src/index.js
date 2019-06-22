8685be157df059b217a032b5a8043952310bfeae
blindthief10 <kostasdiakogiannis23@gmail.com>
2019-06-22 16:53:04 +0200

    fixing duplicate dependency

--- browser/src/index.js
+++ browser/src/index.js
@@ -9,8 +9,6 @@ import { Provider } from 'react-redux';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import { Provider } from 'react-redux';
-import { store } from './redux/redux.js';
 
 ReactDOM.render(<Provider store={store}><App/></Provider>,document.getElementById('root'));
 
