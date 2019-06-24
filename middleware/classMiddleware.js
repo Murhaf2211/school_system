31170f45a97d7783bdf0a22efb9efdbfd15fadcc
blindthief10 <kostasdiakogiannis23@gmail.com>
2019-06-24 09:29:40 +0200

    remove logs

--- middleware/classMiddleware.js
+++ middleware/classMiddleware.js
@@ -35,8 +35,6 @@ const deleteClass = async(req, res, next)=>{
       return res.status(404).json({msg: 'The class you provided does not exist within your school'});
     }
 
-    console.log(findClassByClassCode);
-
     const findRelevantSchool = await schoolModel.findOneAndUpdate({userName: req.user.userName}, {$pull: {courses: findClassByClassCode._id}}, {new: true});
     const updatedPopulatedSchool = await schoolModel.findOne({userName: req.user.userName}).populate('courses', '-_id -school ').select('-_id -password');
     return res.status(200).json({msg: 'Success', schoolInfo: updatedPopulatedSchool});
